f8f50b6f37b4d96d1a435b831dc0d3b1879eb65e
Ildar107 <ildar107wk@gmail.com>
2019-10-13 23:52:38 +0300

    feat: add node

--- src/node.js
+++ src/node.js
@@ -1,22 +1,62 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if(this.left === null)
+		{	
+			this.left = node;
+			this.left.parent = this;
+		}
+		else if(this.right === null)
+		{
+			this.right = node;
+			this.left.parent = this;
+		}
 	}
 
 	removeChild(node) {
+		if( this.left === node)
+			this.left = null;
+		else if(this.right === node)
+			this.right = null;
+		else
+			throw new Error();
 
+		node.parent = null;
 	}
 
 	remove() {
+		if(this.parent === null)
+			return;
 
+		this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
+		if(this.parent === null)
+			return;
 		
+		this.parent.parent = this;
+		// if(this.parent.left === this)
+		// {
+		// 	this.parent.right.parent = this;
+		// 	this.left = this.parent
+		// 	this.right = this.parent.right;
+		// }
+		// else
+		// {
+		// 	this.parent.left.parent = this;
+		// 	this.right = this.parent;
+		// 	this.left = this.parent.left;
+		// }
+		// 	this.parent.left = this.parent.right = null;
+		// 	this.parent = null;
 	}
 }
 
